becd0fe8d8d39839ed2659f7e7fd4129e21a78a6
tomasdelpinorep <tomasdelpino28@gmail.com>
2023-11-09 10:18:08 +0100

    midway

--- FullMovieDB/src/app/components/carousell/carousell.component.ts
+++ FullMovieDB/src/app/components/carousell/carousell.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { PopularMovie } from 'src/app/models/popular-movie-list';
+import { TrendingMovie, TrendingMoviesResponse } from 'src/app/models/trending-movies';
+import { MovieService } from 'src/app/services/movie-service.service';
 
 @Component({
   selector: 'app-carousell',
   templateUrl: './carousell.component.html',
   styleUrls: ['./carousell.component.css']
 })
-export class CarousellComponent {
+export class CarousellComponent implements OnInit{
+	currentPage = 1;
+	movieList: PopularMovie[] = [];
 	showNavigationArrows = true;
 	showNavigationIndicators = false;
 	images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-	constructor(config: NgbCarouselConfig) {
+	constructor(config: NgbCarouselConfig, private movieService :MovieService) {
 		config.showNavigationArrows = true;
 		config.showNavigationIndicators = true;
+	}
+
+	ngOnInit(): void {
+		this.movieService.getPopularMoviesList(this.currentPage).subscribe(resp => {
+			this.movieList = resp.results;
+
+		})
+	}
+
+	getFeaturedMoviesPosterPath(trendingMovieList: TrendingMovie[]){
+		for const m of trendingMovieList{
+			
+		}
 	}
 }
--- FullMovieDB/src/app/services/movie-service.service.ts
+++ FullMovieDB/src/app/services/movie-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { PopularMoviesResponse } from '../models/top-rated-list';
+import { TrendingMoviesResponse } from '../models/trending-movies';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,12 @@ export class MovieService {
   getPopularMoviesList(page: number): Observable<PopularMoviesResponse> {
     return this.http.get<PopularMoviesResponse>(
       `https://api.themoviedb.org/3/movie/popular?page=${page}&api_key=fba6287e1b5585e45727ead4703af755`
+    );
+  }
+
+  getTrendingMoviesList(): Observable<TrendingMoviesResponse> {
+    return this.http.get<TrendingMoviesResponse>(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=fba6287e1b5585e45727ead4703af755`
     );
   }
 }
