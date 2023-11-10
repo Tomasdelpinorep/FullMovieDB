5694acdbdf09bdfbb04ad526d7c8d8975a94886e
Tomasdelpinorep <tomasdelpino28@gmail.com>
2023-11-10 23:58:03 +0100

    arreglado posicionamiento de carousel, navbar y una lista

--- FullMovieDB/src/app/app-routing.module.ts
+++ FullMovieDB/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './ui/home-page/home-page.component';
 import { PageNotFoundComponent } from './ui/page-not-found/page-not-found.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UpcomingMoviesListComponent } from './components/upcoming-movies-list/upcoming-movies-list.component';
 
 const routes: Routes = [
   {path:'home', component:HomePageComponent},
   {path:'', pathMatch:'full',redirectTo:'/home'},
-  {path:'*', component: PageNotFoundComponent}
+  {path:'*', component: PageNotFoundComponent},
+  {path:'upcomingList', component: UpcomingMoviesListComponent}
 ];
 
 @NgModule({
